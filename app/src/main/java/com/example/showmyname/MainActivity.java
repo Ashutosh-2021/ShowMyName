b07f4017ab577ad593a228f64941ec067bb55441
Ashutosh Soni <soniashutosh1980@gmail.com>
2022-01-18 19:59:28 +0530

    Update Code

--- app/src/main/java/com/example/showmyname/MainActivity.java
+++ app/src/main/java/com/example/showmyname/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.showmyname;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+     Button button;
+     TextView textView;
+     EditText etText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button2);
+        textView = findViewById(R.id.textView2);
+        etText = findViewById(R.id.etText);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = etText.getText().toString();
+                textView.setText("hello, "+name);
+            }
+        });
     }
 }
